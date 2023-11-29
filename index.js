e6f037846983c7544606557b46781c49eb7aafe8
Md Tasdid Nayem <selinakhatun982520@gmail.com>
2023-11-30 01:21:05 +0600

    post user collection

--- index.js
+++ index.js
@@ -24,7 +24,20 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+        const userCollection = client.db("parcelDb").collection('users');
         const reviewsCollection = client.db("parcelDb").collection('reviews');
+
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const query = { email: user.email,name:user.name,photo:user.photo }
+            const existingUser = await userCollection.findOne(query);
+            if (existingUser) {
+                return res.send({ message: "user already exists", insertedId: null })
+            }
+            const result = await userCollection.insertOne(user);
+            res.send(result);
+        })
+
         app.get('/reviews', async (req, res) => {
             const result = await reviewsCollection.find().toArray();
             res.send(result);
